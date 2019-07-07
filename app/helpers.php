d7c995dd29f9d13ee830d60fc409a457cd437cb6
guilin <1141768780@qq.com>
2019-07-07 13:22:24 +0000

    分类话题列表

--- app/helpers.php
+++ app/helpers.php
@@ -1,4 +1,14 @@
 <?php
-    function route_class() {
+    function route_class()
+    {
         return str_replace('.','-',\Illuminate\Support\Facades\Route::currentRouteName());
+    }
+
+/**
+ * @param $category_id
+ * @return string
+ */
+    function category_nav_active($category_id)
+    {
+        return active_class((if_route('categories.show') && if_route_param('category',$category_id)));
     }
