e0f0d69825bc0f100cedc3ec1725d3ec7c92be0f
guilin <1141768780@qq.com>
2019-07-14 12:18:57 +0000

    WYSIWYG 编辑器

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -43,7 +43,7 @@
             </div>
 
             <div class="form-group">
-              <textarea name="body" class="form-control" id="editor" rows="6" placeholder="请填入至少三个字符的内容。" required>{{ old('body', $topic->body ) }}</textarea>
+              <textarea name="body" class="form-control" id="editor" rows="6" placeholder="请填入至少三个字符的内容" required>{{ old('body', $topic->body ) }}</textarea>
             </div>
 
             <div class="well well-sm">
@@ -54,5 +54,23 @@
     </div>
   </div>
 </div>
+@endsection
+@section('styles')
+<link rel="stylesheet" type="text/css" href="{{ asset('css/simditor.css') }}">
+@endsection
+
+@section('scripts')
+<script type="text/javascript" src="{{ asset('js/module.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/hotkeys.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/uploader.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/simditor.js') }}"></script>
+
+<script>
+  $(document).ready(function () {
+    var editor = new Simditor({
+      textarea: $('#editor'),
+    });
+  });
+</script>
 
 @endsection
